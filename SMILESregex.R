781c0ab22de4e57d8ecf5d1e04a5378196acf30e
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-12 01:39:15 -0700

    Update SMILESregex.R

--- SMILESregex.R
+++ SMILESregex.R
@@ -6,12 +6,20 @@ apply(str_match_all("C1C(C2(CC1)C)CC2C","C[0-9%\\(]*(?=(C))")[[1]],1,function(x)
 #List positions in string for each match
 
 #List all positions in the match as igraph two-column matrix
-##First step finds all reasonable element names
-gregexpr("[A-Z][a-z]{,2}","C1C(C2(CC1)C)CC2C")
-##Second step sets up matrix of two columns and rownumber equal to the number of letter matches
-dump<-matrix(0,ncol=2,nrow=length(unlist(gregexpr("[A-Z][a-z]{,2}","C1C(C2(CC1)C)CC2C"))))
+##First, define starting position and start of capture position. cbind() values to make a 2 column matrix
 start<-unlist(gregexpr("C[0-9%\\(]*(?=(C))","C1C(C2(CC1)C)CC2C",perl=T))
 capture.start<-attr(gregexpr("C[0-9%\\(]*(?=(C))","C1C(C2(CC1)C)CC2C",perl=T)[[1]],"capture.start")
 links<-cbind(start,capture.start)
-
+#This 2 column matrix should be adequate for graph.edgelist() in the igraph package.
 #List all C-C single bonds in the molecule C1C(C2(CC1)C)CC2C
+#For matching atoms with numbers to show their connectivity, 
+#Display strings with numbers attached
+regmatches("C1C(C2(CC1234)C)C%519%12CC",gregexpr("[A-Z][0-9]+|C(%[0-9]{2,})+","C1C(C2(CC1234)C)C%519%12CC",perl=T))
+#positions of matches
+list1<-list()
+for(i in 1:4){
++ list1[[i]]<-seq(gregexpr("[A-Z][0-9%]+","C1C(C2(CC1234)C)C12%519CC",perl=T)[[1]][i]+1,length=attr(gregexpr("[A-Z][0-9%]+","C1C(C2(CC1234)C)C12%519CC",perl=T)[[1]],"match.length")[i])
++ }
+list1
+#
+#Get all numbers and order them in a vector
